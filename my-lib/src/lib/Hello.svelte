939e4b7aca4df8caa92e11efc7d38d28394e3331
bhvgt <bhavin.kamani@gembatech.com>
2022-04-01 17:13:12 +0530

    Initial commit

---
+++ my-lib/src/lib/Hello.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+	import dayjs from "dayjs";
+</script>
+<div>{dayjs().format("DD-MMM-YYYY hh:mm:ss")}</div>
---
+++ my-lib/src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Hello from "$lib/Hello.svelte";
+</script>
+<h1>Today is <Hello/></h1>
+
